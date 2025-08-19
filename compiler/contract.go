5d3da5298b55d54377bbd1fd544ed0972cf59bca
Qi Zhou <qizhou@ethstorage.io>
2025-08-19 07:48:17 +0000

    add contract.go

---
+++ compiler/contract.go
@@ -0,0 +1,32 @@
+package compiler
+
+import (
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/holiman/uint256"
+)
+
+type Contract struct {
+	caller       common.Address
+	address      common.Address
+	CompiledCode []byte
+
+	CodeHash common.Hash
+
+	Gas   uint64
+	value *uint256.Int
+}
+
+// NewContract returns a new contract environment for the execution of EVM.
+func NewContract(caller common.Address, address common.Address, value *uint256.Int, gas uint64, codeHash common.Hash) *Contract {
+	return &Contract{
+		caller:   caller,
+		address:  address,
+		CodeHash: codeHash,
+		Gas:      gas,
+		value:    value,
+	}
+}
+
+func (c *Contract) SetCompiledCode(compiledCode []byte) {
+	c.CompiledCode = compiledCode
+}
